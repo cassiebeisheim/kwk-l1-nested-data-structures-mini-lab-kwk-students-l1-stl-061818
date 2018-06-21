edca3d36f71cfaa554dae0e6574efdb91b5c4194
cassiebeisheim <cassiebeisheim@gmail.com>
2018-06-21 18:59:06 +0000

    automatically backed up by learn

--- project_calculator.rb
+++ project_calculator.rb
@@ -112,7 +112,43 @@ else anwser == "d"
   total += 4 
 end
 
-
+ puts "What is your favorite food?
+  a.sushi  
+  b.fruit
+  c.burger
+  d.salad "
+  
+  food=gets.chomp
+  
+  if food=="a"
+    answers.push("a")
+  elsif food=="b"
+    answers.push("b")
+  elsif food=="c"
+    answers.push("c")
+  elsif food=="d"
+    answers.push("d")
+    
+  end
+  
+ puts "What is your favorite hobby?
+ a. reading
+ b. drawing
+ c. playing sports
+ d. sightseeing"
+ 
+ hobby=gets.chomp
+ 
+ if hobby=="a"
+    answers.push("a")
+ elsif hobby=="b"
+    answers.push("b")
+ elsif hobby=="c"
+    answers.push("c")
+ elsif hobby=="d"
+    answers.push("d")
+    
+  end
 
 answers.each do |answer|
   if answer == "a"

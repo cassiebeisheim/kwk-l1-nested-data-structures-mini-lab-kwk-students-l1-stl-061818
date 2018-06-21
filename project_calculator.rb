f1db85118ee4da97123c0b1f9408cf5e5ff8257b
cassiebeisheim <cassiebeisheim@gmail.com>
2018-06-21 18:58:44 +0000

    automatically backed up by learn

--- project_calculator.rb
+++ project_calculator.rb
@@ -80,6 +80,40 @@ end
 
 #other questions
 
+total = 0
+q2 = "what word would your friends use to descibe you?/n(a)intellectual/n(b)practical /n(c)passionate/enegetic/n(d)emotional or mysterious"
+
+anwser = gets.chomp 
+puts q2
+
+
+if anwser== "a" 
+  total += 1 
+elsif anwser == "b"
+  total += 2 
+elsif anwser == "c" 
+  total += 3 
+else anwser == "d"
+  total += 4 
+end
+  
+  q3 = "what color do you most associate yourself with or just like?/n(a)pastels+aqua/n(b)light greens + brown/n(c)blues+intense colors/n(d)red+purple"
+  
+   puts q3
+   anwser= gets.chomp
+ 
+  if anwser=="a" 
+  total += 1 
+elsif anwser == "b"
+  total += 2 
+elsif anwser == "c" 
+  total += 3 
+else anwser == "d"
+  total += 4 
+end
+
+
+
 answers.each do |answer|
   if answer == "a"
     total = total + 1
